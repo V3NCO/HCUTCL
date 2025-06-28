357df82ed4307469a6b5488516d3d57e06bd10b4
Venco <v3nco@v3nco.dev>
2025-06-28 16:39:09 +0200

    Added basic converter between languages

--- bolt-app/app.py
+++ bolt-app/app.py
@@ -7,5 +7,31 @@ load_dotenv()
 
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
+@app.command("/hcul-to-brainfuck")
+def hcul_to_brainfuck(ack, respond, command):
+    ack() # Acknowledge request for slack
+    command_text = f"{command['text']}"
+
+    replacement = {":upvote:": "<", ":downvote:": ">", ":yay:": "+", ":heavysob:": "-", ":pf:": ".", ":3c:": ",", ":dino-drake-yea:": "[", ":dino-drake-nah:": "]", " ": ""}
+
+    for o, n in replacement.items():
+        command_text = command_text.replace(o, n)
+
+    respond(f"{command_text}")
+    print(f"{command_text}")
+
+@app.command("/brainfuck-to-hcul")
+def brainfuck_to_hcul(ack, respond, command):
+    ack() # Acknowledge request for slack
+    command_text = f"{command['text']}"
+
+    replacement = {":upvote:": "<", ":downvote:": ">", ":yay:": "+", ":heavysob:": "-", ":pf:": ".", ":3c:": ",", ":dino-drake-yea:": "[", ":dino-drake-nah:": "]"}
+
+    for o, n in replacement.items():
+        command_text = command_text.replace(n, o)
+
+    respond(f"{command_text}")
+    print(f"{command_text}")
+
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
