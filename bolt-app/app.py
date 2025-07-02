98728d9a2caaeba10ac3f470a6d0a3dfbabaabaa
Venco <v3nco@v3nco.dev>
2025-07-02 20:39:31 +0200

    Add Block kit for commands

--- bolt-app/app.py
+++ bolt-app/app.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import logging
 from sys import stdout
+import json
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from hcul import InputFieldCountException, NoInputFieldException, hcul
@@ -12,7 +13,7 @@ except:
 
 logger = logging.getLogger('mylogger')
 
-logger.setLevel(logging.DEBUG) # set logger level
+logger.setLevel(logging.INFO) # set logger level
 logFormatter = logging.Formatter\
 ("%(name)-12s %(asctime)s %(levelname)-8s %(filename)s:%(funcName)s %(message)s")
 consoleHandler = logging.StreamHandler(stdout) #set streamhandler to stdout
@@ -35,11 +36,22 @@ def hcul_to_brainfuck(ack, respond, command):
 
     logger.info(f"Input : {command['text']}")
     logger.info(f"Output : {command_text}")
-    respond(f"{command_text}")
+    user = app.client.users_info(user=command['user_id'])
+    respond(
+        {
+            "blocks" : [
+                {"type": "context","elements": [{"type": "image","image_url": f"{user['user']['profile']['image_32']}","alt_text": f"{user['user']['profile']['display_name']}'s pfp"},{"type": "mrkdwn","text": f"*{user['user']['profile']['display_name']}* Asked: "}]},
+                {"type": "section", "text": {"type": "mrkdwn", "text": f">:idea-dino: Convert this Hackclub Universal Langugage code into Brainfuck :\n{command['text']}"}},
+                {"type": "divider"},
+                {"type": "section", "text": {"type": "mrkdwn", "text": f"Output :\n{command_text}"}}
+            ],
+            "response_type": "in_channel"
+        }
+    )
 
 @app.command("/brainfuck-to-hcul")
-def brainfuck_to_hcul(ack, respond, command):
-    ack() # Acknowledge request for slack
+def brainfuck_to_hcul(ack, respond, command, body, client):
+    ack()
     logger.info(f"Received {command} and acknowledged it")
     command_text = f"{command['text']}"
 
@@ -49,7 +61,23 @@ def brainfuck_to_hcul(ack, respond, command):
         command_text = command_text.replace(n, o)
     logger.info(f"Input : {command['text']}")
     logger.info(f"Output : {command_text}")
-    respond(f"{command_text}")
+    user = app.client.users_info(user=command['user_id'])
+    respond(
+        {
+            "blocks" : [
+                {"type": "context","elements": [{"type": "image","image_url": f"{user['user']['profile']['image_32']}","alt_text": f"{user['user']['profile']['display_name']}'s pfp"},{"type": "mrkdwn","text": f"*{user['user']['profile']['display_name']}* Asked: "}]},
+                {"type": "section", "text": {"type": "mrkdwn", "text": f">:idea-dino: Convert this Brainfuck code into Hackclub Universal Language :\n{command['text']}"}},
+                {"type": "divider"},
+                {"type": "section", "text": {"type": "mrkdwn", "text": f"Output :\n{command_text}"}}
+            ],
+            "response_type": "in_channel"
+        }
+    )
+
+@app.view("view-id")
+def view_submission(ack, body, logger):
+    ack()
+    logger.info(body["view"]["state"]["values"])
 
 @app.command("/run-hcutcl")
 def run_code(ack, respond, command):
